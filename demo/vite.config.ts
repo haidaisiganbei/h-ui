ca42f779766c6ed11e296fac08f1c3bd10ad098f
海带丝干杯 <33407215+haidaisiganbei@users.noreply.github.com>
2024-01-21 17:27:22 +0800

    feat: 增加ts支持

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { join } from 'node:path'
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: /^@hui\/(.+)$/, 
+        replacement: join(__dirname, '..', 'packages', '$1', 'src') 
+      },
+    ]
+  }
 })
